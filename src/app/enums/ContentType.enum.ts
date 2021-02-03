880eb1d50a90ce7ff33a7414d5e8e2f0969978db
Emilio Barboza <pringles212@gmail.com>
2021-02-03 16:25:00 -0400

    feat: add at-iframe

--- src/app/enums/ContentType.enum.ts
+++ src/app/enums/ContentType.enum.ts
@@ -3,6 +3,7 @@ export enum ContentType {
   AT_BUTTON = 'at-button',
   ML_RICH_TEXT = 'ml-rich-text',
   ML_HEADING = 'ml-heading',
+  AT_IFRAME = 'at-iframe',
   OR_FOOTER = 'or-footer',
   OR_HEADER = 'or-header',
 }
